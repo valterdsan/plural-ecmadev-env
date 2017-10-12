1bc5d39d289a9914bf7f2006423b9ca463a67a7e
valterdsan <wdsrye@gmail.com>
2017-10-12 10:47:16 -0300

    Bundling and Sourcemaps

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,10 +3,18 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-const port = 3000;
-// now lets create an instance of express;
-const app = express();
+const port = 3000;       // now lets create an instance of express;
+const app  = express();  // now I'm integrating Webpack with Express
+const compiler = webpack(config);
+
+// now I'm telling Express other things that I'd like to use
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
